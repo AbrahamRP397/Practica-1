e5a597f8b12615116ec41c629ba8b748f62827b4
AbrahamRP397 <20233tn162@utez.edu.mx>
2025-05-12 09:56:58 -0600

    [ADD] se implemento un programa que pide la edad

--- App.java
+++ App.java
@@ -3,6 +3,9 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu edad: ");
+        int edad = sc.nextInt();
+        System.out.println("Tu edad es: " + edad);
     }
 }
